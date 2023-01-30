9e0e19d89ac71ded992fc6d74a0f67696e72c4c1
Mytsu <tlc.jooker@gmail.com>
2023-01-30 16:50:22 -0300

    fix get chapter title variable location and format

--- src/scrappers/boxnovel.rs
+++ src/scrappers/boxnovel.rs
@@ -2,25 +2,22 @@ use crate::novel::{Chapter, Novel};
 use crate::utils::scraper::{get_content, get_element_ref, get_selector};
 use reqwest::blocking::{get, Client};
 use scraper::{Html, Selector};
-use std::{time, thread};
+use std::{thread, time};
 
 pub fn get_novel(url: &str) -> Novel {
     let body = get_response(url);
-    let cover_url = get_cover_url(&body);
-    let title = get_novel_title(&body);
-    let synopsis = get_novel_synopsis(&body);
-    let authors = get_novel_author(&body);
     let mut chapters = Vec::<Chapter>::new();
-    let chapter_links = fetch_chapter_links(url);
+    let mut chapter_links = fetch_chapter_links(url);
+    chapter_links.reverse();
     for link in chapter_links {
         chapters.push(get_chapter(&link));
         thread::sleep(time::Duration::from_millis(500));
     }
     Novel::new(
-        title,
-        synopsis,
-        authors,
-        cover_url,
+        get_novel_title(&body),
+        get_novel_synopsis(&body),
+        get_novel_author(&body),
+        get_cover_url(&body),
         url.to_string(),
         chapters,
     )
@@ -28,18 +25,16 @@ pub fn get_novel(url: &str) -> Novel {
 
 pub fn get_cover_url(body: &str) -> String {
     let selector = get_selector(".summary_image > a > .img-responsive");
-    let fragment = Html::parse_fragment(body);
-    fragment
+    Html::parse_fragment(body)
         .select(&selector)
         .flat_map(|el| el.value().attr("src"))
         .collect()
 }
 
 pub fn get_novel_title(body: &str) -> String {
-    // TODO: parse and remove artifacts like: \n \t
     let title = get_element_content(".post-title > h1", body);
     println!("Fetching: {}", title);
-    title
+    title.replace( "\n", "").replace("\t", "")
 }
 
 pub fn get_novel_synopsis(body: &str) -> Vec<String> {
@@ -53,7 +48,8 @@ pub fn get_novel_author(body: &str) -> Vec<String> {
 }
 
 pub fn get_chapter_links(body: &str) -> Vec<String> {
-    let selector = Selector::parse("li.wp-manga-chapter > a").unwrap();
+    let selector = Selector::parse("li.wp-manga-chapter > a")
+        .unwrap();
     let fragment = Html::parse_document(body);
     let mut links = Vec::<String>::new();
     for el in fragment.select(&selector) {
@@ -66,13 +62,14 @@ pub fn get_chapter_links(body: &str) -> Vec<String> {
 pub fn get_chapter(url: &str) -> Chapter {
     let body = get_response(url);
     let title = get_chapter_title(&body);
-    let content = get_chapter_content(&body);
     println!("{}", title);
-    Chapter::new(title, content)
+    Chapter::new(title, get_chapter_content(&body))
 }
 
 pub fn get_chapter_title(body: &str) -> String {
-    get_element_content(".text-left > h3", body)
+    get_element_content(".breadcrumb li.active", body)
+        .replace("\n", "")
+        .replace("\t", "")
 }
 
 pub fn get_chapter_content(body: &str) -> Vec<String> {
@@ -81,8 +78,7 @@ pub fn get_chapter_content(body: &str) -> Vec<String> {
 
 pub fn get_response(url: &str) -> String {
     println!("Url: {}", url);
-    let response = get(url).unwrap();
-    response.text().unwrap()
+    get(url).unwrap().text().unwrap()
 }
 
 pub fn get_element_content(tag: &str, body: &str) -> String {
