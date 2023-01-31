da4da6894a2d274b0e83b7b2e7132558e3df1ff5
Mytsu <tlc.jooker@gmail.com>
2023-01-31 20:53:07 -0300

    add url crate to handle parsing

--- src/scrappers/boxnovel.rs
+++ src/scrappers/boxnovel.rs
@@ -3,14 +3,16 @@ use crate::utils::scraper::{get_content, get_element_ref, get_selector};
 use reqwest::blocking::{get, Client};
 use scraper::{Html, Selector};
 use std::{thread, time};
+use url::Url;
 
-pub fn get_novel(url: &str) -> Novel {
-    let body = get_response(url);
+pub fn get_novel(input: &str) -> Novel {
+    let url = Url::parse(input).unwrap();
+    let body = get_response(&url);
     let mut chapters = Vec::<Chapter>::new();
-    let mut chapter_links = fetch_chapter_links(url);
+    let mut chapter_links = fetch_chapter_links(&url);
     chapter_links.reverse();
     for link in chapter_links {
-        chapters.push(get_chapter(&link));
+        chapters.push(get_chapter(&Url::parse(&link).unwrap()));
         thread::sleep(time::Duration::from_millis(500));
     }
     Novel::new(
@@ -18,7 +20,7 @@ pub fn get_novel(url: &str) -> Novel {
         get_novel_synopsis(&body),
         get_novel_author(&body),
         get_cover_url(&body),
-        url.to_string(),
+        input.to_string(),
         chapters,
     )
 }
@@ -34,7 +36,7 @@ pub fn get_cover_url(body: &str) -> String {
 pub fn get_novel_title(body: &str) -> String {
     let title = get_element_content(".post-title > h1", body);
     println!("Fetching: {}", title);
-    title.replace( "\n", "").replace("\t", "")
+    title.replace("\n", "").replace("\t", "")
 }
 
 pub fn get_novel_synopsis(body: &str) -> Vec<String> {
@@ -48,8 +50,7 @@ pub fn get_novel_author(body: &str) -> Vec<String> {
 }
 
 pub fn get_chapter_links(body: &str) -> Vec<String> {
-    let selector = Selector::parse("li.wp-manga-chapter > a")
-        .unwrap();
+    let selector = Selector::parse("li.wp-manga-chapter > a").unwrap();
     let fragment = Html::parse_document(body);
     let mut links = Vec::<String>::new();
     for el in fragment.select(&selector) {
@@ -59,7 +60,7 @@ pub fn get_chapter_links(body: &str) -> Vec<String> {
     links
 }
 
-pub fn get_chapter(url: &str) -> Chapter {
+pub fn get_chapter(url: &Url) -> Chapter {
     let body = get_response(url);
     let title = get_chapter_title(&body);
     println!("{}", title);
@@ -76,9 +77,8 @@ pub fn get_chapter_content(body: &str) -> Vec<String> {
     get_content(".text-left > p", body)
 }
 
-pub fn get_response(url: &str) -> String {
-    println!("Url: {}", url);
-    get(url).unwrap().text().unwrap()
+pub fn get_response(url: &Url) -> String {
+    get(url.to_string()).unwrap().text().unwrap()
 }
 
 pub fn get_element_content(tag: &str, body: &str) -> String {
@@ -92,9 +92,8 @@ pub fn get_element_content(tag: &str, body: &str) -> String {
         .to_owned()
 }
 
-pub fn fetch_chapter_links(url: &str) -> Vec<String> {
-    // TODO: Handle if url has no / at the end
-    let new_url = vec![url, "ajax/chapters"].join("");
+pub fn fetch_chapter_links(url: &Url) -> Vec<String> {
+    let new_url = url.join("ajax/chapters").unwrap().to_string(); //vec![url, "ajax/chapters"].join("");
     let client = Client::new();
     let body = client.post(&new_url).send().unwrap().text().unwrap();
     let links = get_chapter_links(&body);
